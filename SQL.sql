2dae3d7c84d08a6833cf17464007c0509fa3cdc2
Diego Dias Jacobino <109692760+DiegoDJacobino@users.noreply.github.com>
2023-01-27 15:52:55 -0300

    Add files via upload

---
+++ SQL.sql
@@ -0,0 +1,28 @@
+SELECT  p.BusinessEntityID, 
+        p.Title, 
+        p.FirstName, 
+        p.MiddleName, 
+        p.LastName, 
+        p.Suffix, 
+        pp.PhoneNumber, 
+        pnt.Name AS PhoneNumberType, 
+        ea.EmailAddress, 
+        p.EmailPromotion, 
+        at.Name AS AddressType, 
+        a.AddressLine1, 
+        a.AddressLine2, a.City, 
+        sp.Name AS StateProvinceName, 
+        a.PostalCode, 
+        cr.Name AS CountryRegionName, 
+        p.Demographics
+FROM     Person.Person AS p 
+INNER JOIN Person.BusinessEntityAddress AS bea ON bea.BusinessEntityID = p.BusinessEntityID 
+INNER JOIN Person.Address AS a ON a.AddressID = bea.AddressID 
+INNER JOIN Person.StateProvince AS sp ON sp.StateProvinceID = a.StateProvinceID 
+INNER JOIN Person.CountryRegion AS cr ON cr.CountryRegionCode = sp.CountryRegionCode 
+INNER JOIN Person.AddressType AS at ON at.AddressTypeID = bea.AddressTypeID 
+INNER JOIN Sales.Customer AS c ON c.PersonID = p.BusinessEntityID 
+LEFT OUTER JOIN Person.EmailAddress AS ea ON ea.BusinessEntityID = p.BusinessEntityID 
+LEFT OUTER JOIN Person.PersonPhone AS pp ON pp.BusinessEntityID = p.BusinessEntityID 
+LEFT OUTER JOIN Person.PhoneNumberType AS pnt ON pnt.PhoneNumberTypeID = pp.PhoneNumberTypeID
+WHERE  (c.StoreID IS NULL)
